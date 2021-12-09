fcf3e99e94fc1cce32718949448fe87b2a5f8bdb
umairshaikh5521 <uskofficial5521@gmailcom>
2021-12-09 14:14:19 +0530

    styled header

---
+++ header.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package neevfund
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- <link rel="profile" href="https://gmpg.org/xfn/11"> -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Lato:wght@700&family=Roboto&display=swap" rel="stylesheet">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'neevfund' ); ?></a>
+
+	<header id="masthead" class="site-header">
+		
+		<nav class="navbar navbar-expand-lg navbar-light  main-navigation">
+			<div class="container">
+				<?php the_custom_logo(); ?>
+				<!-- <a class="navbar-brand" href="#">Navbar scroll</a> -->
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarScroll">
+					<div class="ms-auto">
+	
+						<?php
+							wp_nav_menu(
+								array(
+									'theme_location' => 'menu-1',
+									'menu_id'        => 'primary-menu',
+								)
+							);
+						?>
+					</div>
+			
+				</div>
+			</div>
+			</nav>
+	</header><!-- #masthead -->
